9bc460072d98b528338be6b12512b563b6a419f5
syahrul25arul <syahrul25arul@gmail.com>
2022-10-05 23:26:11 +0700

    part-7 belajar http header

---
+++ header_test.go
@@ -0,0 +1,51 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+/*
+* http header tidak case sensitive
+ */
+
+func RequestHeader(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("content-type") // menangkap request header
+	fmt.Fprint(w, contentType)
+}
+
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8000/", nil)
+	request.Header.Add("Content-type", "application/json") // mengirim request header
+
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func ResponseHeader(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("X-Powered-By", "Hendrik") // menangkap response header
+	fmt.Fprint(w, "OK")
+}
+
+func TestResponseHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8000/", nil)
+
+	recorder := httptest.NewRecorder()
+
+	ResponseHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+	fmt.Println(response.Header.Get("x-powered-by")) // menangkap response header
+}
