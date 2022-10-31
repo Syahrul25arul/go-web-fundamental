551704c9a29ab6a22b8f4ea5fe454698bdf29a98
syahrul25arul <syahrul25arul@gmail.com>
2022-10-31 17:54:22 +0700

    part-14 belajar template data

---
+++ template_data_test.go
@@ -0,0 +1,64 @@
+package golang_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateDataMap(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/name.gohtml"))
+	t.ExecuteTemplate(w, "name.gohtml", map[string]interface{}{
+		"Title": "Template data map",
+		"Name":  "Hendrik",
+		"Address": map[string]interface{}{
+			"Street": "Jalan Belum Ada Lagi",
+		},
+	})
+}
+
+func TestTemplateDataMap(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataMap(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+
+	fmt.Println(string(body))
+}
+
+type Address struct {
+	Street string
+}
+
+type Page struct {
+	Address Address
+	Title   string
+	Name    string
+}
+
+func TemplateDataStruct(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/name.gohtml"))
+	t.ExecuteTemplate(w, "name.gohtml", Page{
+		Title: "Template data struct",
+		Name:  "Hendrik",
+		Address: Address{
+			Street: "Jalan Belum Ada",
+		},
+	})
+}
+
+func TestTemplateDataStruct(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataStruct(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+
+	fmt.Println(string(body))
+}
