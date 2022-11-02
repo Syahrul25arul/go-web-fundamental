e29060c915de3ca95666a547c9ccce71b3856f1b
syahrul25arul <syahrul25arul@gmail.com>
2022-11-02 17:06:29 +0700

    part-20 belajar upload form

---
+++ upload_form_test.go
@@ -0,0 +1,79 @@
+package golang_web
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func UploadForm(w http.ResponseWriter, r *http.Request) {
+	myTemplates.ExecuteTemplate(w, "upload.form.gohtml", nil)
+}
+
+func Upload(w http.ResponseWriter, r *http.Request) {
+	// r.ParseMultipartForm(100) // berarti 100 mb max
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	fileDestination, err := os.Create("./resources/" + fileHeader.Filename)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = io.Copy(fileDestination, file)
+	if err != nil {
+		panic(err)
+	}
+	name := r.PostFormValue("name")
+	myTemplates.ExecuteTemplate(w, "upload.success.gohtml", map[string]interface{}{
+		"Title": "upload form",
+		"Name":  name,
+		"File":  "/static/" + fileHeader.Filename,
+	})
+}
+
+func TestUploadForm(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", UploadForm)
+	mux.HandleFunc("/upload", Upload)
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./resources"))))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/CELERATES.jpg
+var uploadFileTest []byte
+
+func TestUploadFile(t *testing.T) {
+	body := new(bytes.Buffer)
+
+	writer := multipart.NewWriter(body)
+	writer.WriteField("name", "hendrik")
+	file, _ := writer.CreateFormFile("file", "contoh_upload.png")
+	file.Write(uploadFileTest)
+	writer.Close()
+
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/upload", body)
+	request.Header.Set("Content-type", writer.FormDataContentType()) // set header content-type multipart/form-data
+	recorder := httptest.NewRecorder()
+
+	Upload(recorder, request)
+
+	bodyResponse, _ := io.ReadAll(recorder.Result().Body)
+
+	fmt.Println(string(bodyResponse))
+}
